286faa9596938c71e9f5d1cf74f6ec6d9b4911aa
evgeniy-tropin <evgeniy.tropin@flexaspect.com>
2024-05-25 21:24:29 +0300

    First commit

---
+++ hw/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Card from './components/Card.vue'
+</script>
+
+<template>
+  <div class="container py-4 px-3 mx-auto">
+    <Card msg="Vite + Vue" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
