1f3b05cf1a0fee323edc2a2672f2b74800b02d0a
evgeniy-tropin <evgeniy.tropin@flexaspect.com>
2024-06-05 10:41:06 +0300

    BtnGroup

--- hw/src/App.vue
+++ hw/src/App.vue
@@ -1,31 +1,16 @@
 <template>
   <div class="container py-4 px-3 mx-auto">
-    <Definitions :data="definitions" />
+    <BtnGroup />
   </div>
 </template>
 
 <script>
-import Definitions from './components/Definitions.vue'
+import BtnGroup from './components/BtnGroup.vue'
 
 export default {
   name: 'App',
   components: {
-    Definitions,
-  },
-  data() {
-    return {
-      definitions: [{
-          dt: 'one',
-          dd: 'two',
-          id: 1
-        },
-        {
-          dt:'another term',
-          dd: 'another description',
-          id: 2
-        },
-      ],
-    };
+    BtnGroup,
   },
 }
 </script>
---
+++ hw/src/components/BtnGroup.vue
@@ -0,0 +1,42 @@
+<template>
+  <div
+    class="btn-group"
+    role="group"
+  >
+    <button
+      type="button"
+      class="btn btn-secondary left"
+      :class="{ 'active' : isLeftBtnActive }"
+      @click="toggleLeftButton()"
+    >Left</button>
+    <button
+      type="button"
+      class="btn btn-secondary right"
+      :class="{ 'active' : isRightBtnActive }"
+      @click="toggleRightButton()"
+    >Right</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BtnGroup',
+
+  data() {
+    return {
+      isLeftBtnActive: false,
+      isRightBtnActive: false,
+    };
+  },
+  methods: {
+    toggleLeftButton() {
+      this.isLeftBtnActive = true
+      this.isRightBtnActive = false
+    },
+    toggleRightButton() {
+      this.isRightBtnActive = true
+      this.isLeftBtnActive = false
+    },
+  },
+}
+</script>
