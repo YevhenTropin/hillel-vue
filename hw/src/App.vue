360b5647af6950bf96a19a1219a0b645410ea8e3
evgeniy-tropin <evgeniy.tropin@flexaspect.com>
2024-06-15 20:20:13 +0300

    Collapse

--- hw/src/App.vue
+++ hw/src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div class="container py-4 px-3 mx-auto">
-    <Carousel
-      :data="imageData"
+    <h1>Приклад компонента Collapse</h1>
+    <Collapse
+      text="Це прихований текст"
+      :opened="true"
     />
   </div>
 </template>
 
 <script>
-import Carousel from './components/Carousel.vue'
+import Collapse from './components/Collapse.vue'
 
 export default {
   name: 'App',
   components: {
-    Carousel,
-  },
-  data() {
-    return {
-      imageData: [],
-    };
-  },
-  mounted () {
-    fetch('https://jsonplaceholder.typicode.com/photos/')
-        .then((response) => response.json())
-        .then((json) => this.imageData = json.slice(0, 3));
+    Collapse,
   },
 }
 </script>
---
+++ hw/src/components/Collapse.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <p>
+      <a
+        class="btn btn-primary"
+        data-bs-toggle="collapse"
+        href="#"
+        role="button"
+        aria-expanded="false"
+        @click.prevent="toggleCollapse"
+      >Link with href</a>
+    </p>
+    <div
+      class="collapse"
+      :class="{ 'show' : isCollapseOpen }"
+    >
+      <div class="card card-body">
+        {{ text }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Collapse',
+  props: {
+    text: {
+      type: String,
+      required: true,
+      default: 'collapse me',
+    },
+    opened: {
+      type: Boolean,
+      required: true,
+      default: true,
+    },
+  },
+  data() {
+    return {
+      isCollapseOpen: this.opened,
+    }
+  },
+  methods: {
+    toggleCollapse() {
+      this.isCollapseOpen = !this.isCollapseOpen
+    },
+  }
+}
+</script>
